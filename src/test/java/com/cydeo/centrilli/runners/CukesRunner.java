df659b73c7d1d8269d3772a6bef48e35d066a819
Saadet <99473429+sakoyaespana@users.noreply.github.com>
2022-07-12 18:38:35 +0200

    test1

--- src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
+++ src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/centrilli/step_definitions",
         dryRun = false,
-        tags = "@saadet"
+        tags = "@"
 )
 public class CukesRunner {
 }
