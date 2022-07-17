3d51b5069926b1d94bf3adfd43a9ebd97cb85c13
fation1992 <fationshaliu@gmail.com>
2022-07-17 14:03:13 +0200

    few modification

--- src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
+++ src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
@@ -12,13 +12,14 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json",
 
         },
 
         features = "src/test/resources/features",
         glue = "com/cydeo/centrilli/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@fation"
 )
 public class CukesRunner {
 }
