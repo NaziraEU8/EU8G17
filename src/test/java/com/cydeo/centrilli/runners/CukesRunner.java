999bc363101bf421e1c4887eeb49719e549eb925
User <vilson.shehu@outlook.com>
2022-07-18 23:47:12 +0200

    TWL-240

--- src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
+++ src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/centrilli/step_definitions",
         dryRun = false,
  
-        tags = ""
+        tags = "@roksi"
 
 )
 public class CukesRunner {
